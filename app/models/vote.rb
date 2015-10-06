68e3522c220c52288cc9adf6cfbd22d7eec15e0b
jamesfwz <james@futureworkz.com>
2015-10-06 11:31:08 +0700

    update vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :our_joke
 
   validates :session_code,  presence: true
-  validates :our_joke_id,   presence: true
-  validates :like,          presence: true
+  validates :our_joke_id,   presence: true, uniqueness: { scope: :session_code,
+                                                          message: "You cannot vote 2 times." }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,7 +4,6 @@ describe Vote do
   describe 'validations' do
     it { is_expected.to validate_presence_of :session_code }
     it { is_expected.to validate_presence_of :our_joke_id }
-    it { is_expected.to validate_presence_of :like }
   end
 
   describe 'associations' do 
